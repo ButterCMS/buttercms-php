2b7f6eb9d585b99f9ee456d237a88fb7c9df423e
Josh Holat <jholat@gmail.com>
2016-10-31 19:19:26 -0700

    update tests now that composer is better

--- tests/main.php
+++ tests/main.php
@@ -2,15 +2,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Easier for local dev to override the latest commit with the local version of the class
-require_once __DIR__ . '/../src/ButterCMS.php';
-require_once __DIR__ . '/../src/Model/Model.php';
-require_once __DIR__ . '/../src/Model/Author.php';
-require_once __DIR__ . '/../src/Model/Category.php';
-require_once __DIR__ . '/../src/Model/Post.php';
-require_once __DIR__ . '/../src/Model/PostResponse.php';
-require_once __DIR__ . '/../src/Model/PostsResponse.php';
-
 use ButterCMS\ButterCMS;
 
 $butterCms = new ButterCMS('fe7098cde166d497fbac00bc5d1b94cc8ce51d0f');
